cd269be84a8a94e6c426af43e86746c234a79fff
yuan20406 <>
2025-09-16 19:53:31 +0800

    1111

--- api/platform-timeout.js
+++ api/platform-timeout.js
@@ -1,7 +1,7 @@
 
 export default async function handler(req, res) {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 35000));
+    await new Promise((resolve) => setTimeout(resolve, 65000));
     res.status(200).send('Task Completed');
   } catch (error) {
     res.status(500).send('Internal Server Error');
